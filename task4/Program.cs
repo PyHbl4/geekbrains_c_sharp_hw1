0cbf456e0f84a22184b018af5fcfc818388e3edc
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 01:33:33 +0500

    finish task4

---
+++ task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите натуральное положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < num; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + "  ");
+    }
+}
