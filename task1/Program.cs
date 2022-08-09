f49b164067ff35e1594a6d621765fc1f48c1f155
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 00:49:47 +0500

    finish task1

---
+++ task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значение переменной a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.WriteLine("Переменная a (" + a + ") больше, чем переменная b (" + b + ")");
+}
+else
+{
+        Console.WriteLine("Переменная b (" + b + ") больше, чем переменная a (" + a + ")");
+}
