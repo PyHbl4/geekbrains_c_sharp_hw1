409b74f9cf04af505c2c8ce713db20b75d49cf64
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 01:23:49 +0500

    finish task3

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число для проверки на четность");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.WriteLine("Введенное число - четное");
+}
+else
+{
+    Console.WriteLine("Введенное число - нечетное");
+}
