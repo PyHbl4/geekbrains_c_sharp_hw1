51b7c19d2d910097ea043bbcded03c58d5be9237
PyHbl4 <maxim_runkov@mail.ru>
2022-08-10 01:20:05 +0500

    finish task2

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите значение переменной a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение переменной c");
+int c = Convert.ToInt32(Console.ReadLine());
+if ((a > b) & (a > c))
+{
+    Console.WriteLine("Наибольшее значение у переменной a: " + a);
+} 
+else if ((b > a) & (b > c))
+{
+    Console.WriteLine("Наибольшее значение у переменной b: " + b);
+}
+else
+{
+    Console.WriteLine("Наибольшее значение у переменной c: " + c);
+}
